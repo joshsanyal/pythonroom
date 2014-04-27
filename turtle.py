b7c93b4d655ef7eab343be77568a50356b56c90a
joshsanyal <josh.sanyal@yahoo.com>
2014-04-27 15:33:37 -0700

    pythonroom update

--- turtle.py
+++ turtle.py
@@ -1,1 +1,13 @@
-# author: joshsanyal
+import turtle
+
+josh = turtle.Turtle()
+
+josh.forward(100)
+josh.left(90)
+josh.forward(100)
+josh.left(90)
+josh.forward(100)
+josh.left(90)
+josh.forward(100)
+josh.left(90)
+josh.forward(50)
