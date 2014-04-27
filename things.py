b6e462805d93f0bbeea1bef964c2beca0b17c6b6
joshsanyal <josh.sanyal@yahoo.com>
2014-04-27 13:47:10 -0700

    pythonroom update

--- things.py
+++ things.py
@@ -1,1 +1,10 @@
 # author: joshsanyal
+
+thingstodo = ["watch a Rockets game", "visit the southern hemisphere", "go camping"]
+
+for thing in thingstodo:
+	answer = input ( "Would you want to " + thing + "?" )
+	if answer == "yes":
+		print "Then do " + thing
+	if answer == "no":
+		print "Then don't " + thing
