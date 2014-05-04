919e84a8cef11d3250c8816ea0585117d7a54a15
joshsanyal <josh.sanyal@yahoo.com>
2014-05-04 15:08:44 -0700

    pythonroom update

--- fridaythe13th.py
+++ fridaythe13th.py
@@ -8,13 +8,13 @@ for year in years:
 		days = range(1,32)
 		if month == 2:
 			days = range(1,29)
-			if year % 4 == 0:
-				days = range(1,30)
-			if year % 100 == 0:
-				days = range(1,29)
 			if year % 400 == 0:
 				days = range(1,30)
-					
+			elif year % 100 == 0:
+				days = range(1,29)
+			elif year % 4 == 0:
+				days = range(1,30)
+				
 		if month == 4 or month == 6 or month == 9 or month == 11:
 			days = range(1,31)
 			
