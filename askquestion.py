97b35fa4bb63b978e2327bcce9eb43dfb50b9adb
joshsanyal <josh.sanyal@yahoo.com>
2014-05-04 15:09:59 -0700

    initial commit

---
+++ askquestion.py
@@ -0,0 +1,24 @@
+# author: joshsanyal
+years = range(1900,2015)
+months = range(1,13)
+total = 0
+
+for year in years:
+	for month in months:
+		days = range(1,32)
+		if month == 2:
+			days = range(1,29)
+			if year % 400 == 0:
+				days = range(1,30)
+			elif year % 100 == 0:
+				days = range(1,29)
+			elif year % 4 == 0:
+				days = range(1,30)
+				
+		if month == 4 or month == 6 or month == 9 or month == 11:
+			days = range(1,31)
+			
+		for day in days:
+			total = total + 1
+	
+print total
