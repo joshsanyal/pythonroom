f87ca8a1577a13d1e59f4db3af98f3fd246d1c8d
joshsanyal <josh.sanyal@yahoo.com>
2014-05-04 15:33:43 -0700

    pythonroom update

--- questions.py
+++ questions.py
@@ -3,7 +3,23 @@ question = input("Speak to me.")
 question = question.lower()
 word = question.split()
 
-if word[0] == "who":
+if word[0] == "what":
+	if word[1] == "are":
+		if word[2] == "you?":
+			print "I am a robot named Awesomeness 10000"
 	if word[1] == "is":
-		if word[2] == "
-		
+		if word[3] == "a"
+			if word[4] == "robot?"
+				print "A robot is a robot"
+if word[0] == "where":
+	if word[1] == "do":
+		if word[2] == "you":
+			if word[3] == "live?"
+				print "I live in the planet Krypton, home to Superman."
+if word[0] == "how"
+	if word[1] == "does"
+		if word[2] == "a"
+			if word[3] == "plant"
+				if word[4] == "create"
+					if word[5] == "energy?"
+						print "It uses photosynthesis to create sugar thet is needed to survive."
