ccc1efc486ab8cf57559b53898c95a90377c8650
joshsanyal <josh.sanyal@yahoo.com>
2014-05-11 14:18:43 -0700

    pythonroom update

--- Star.py
+++ Star.py
@@ -1,13 +1,18 @@
 # author: joshsanyal
 import turtle
-t = turtle.Turtle()
-
-t.forward(100)
-t.left(144)
-t.forward(100)
-t.left(144)
-t.forward(100)
-t.left(144)
-t.forward(100)
-t.left(144)
-t.forward(100)
+t1 = turtle.Turtle()
+world=turtle.Screen()
+world.bgcolor("pink")
+t1.color("purple")
+t1.hideturtle()
+t2 = turtle.Turtle()
+t2.color("purple")
+t2.hideturtle()
+t1.left(30)
+t2.left(150)
+for i in range(22):
+	t1.left(i)
+	t1.forward(12)
+for j in range(22):
+	t2.right(j)
+	t2.forward(12)
