7c71322f65603a060fc53a5aba4a12dfc73c8554
joshsanyal <josh.sanyal@yahoo.com>
2014-05-11 15:36:05 -0700

    pythonroom update

--- primesum.py
+++ primesum.py
@@ -1,6 +1,13 @@
 # author: joshsanyal
-numbers = range(2,1001)
+# 1. Average distance
+# 2. Largest distance
+# 3. Smallest distance
+# 4. How many twin primes
+# 5. Sum of the distance between twin primes
+
 total = 0
+lastPrime = 2
+numbers = range(2,1001)
 for n in numbers:
 	tests = range(2,n-1)
 	prime = True
@@ -8,7 +15,6 @@ for n in numbers:
 		if n % t == 0:
 			prime = False
 	if prime:
-		total = total + n
-		
+		total = total + ( n - lastPrime)
+		lastPrime = n	
 print total
-		
