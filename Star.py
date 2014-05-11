6088cfb3736b4783483f314fb96495b5bc38d2a9
joshsanyal <josh.sanyal@yahoo.com>
2014-05-11 13:26:08 -0700

    pythonroom update

--- Star.py
+++ Star.py
@@ -1,1 +1,13 @@
 # author: joshsanyal
+import turtle
+t = turtle.Turtle()
+
+t.forward(100)
+t.left(144)
+t.forward(100)
+t.left(144)
+t.forward(100)
+t.left(144)
+t.forward(100)
+t.left(144)
+t.forward(100)
