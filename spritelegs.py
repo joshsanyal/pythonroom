700aa5794da673b25dfc2b262b0805dbbdcb15c1
joshsanyal <josh.sanyal@yahoo.com>
2014-05-12 16:06:14 -0700

    pythonroom update

--- spritelegs.py
+++ spritelegs.py
@@ -6,11 +6,14 @@ t = turtle.Turtle()
 n = int (input("How many legs should this sprite have?"))
 print n
 
-t.shape("circle")
-angle = 360 / n
+t.shape("triangle")
+angle = 352 / n
 	
 for j in range(0,n):
 	t.right(angle)
 	t.forward(100)
 	t.stamp()
+	for n in range(18):
+		t.right(22)
+		t.stamp()
 	t.backward(100)
