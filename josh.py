1dff989c9443c4ebaed6aa74020c5c1f052921e8
joshsanyal <josh.sanyal@yahoo.com>
2014-05-12 16:07:05 -0700

    pythonroom update

--- josh.py
+++ josh.py
@@ -1,19 +1,6 @@
 # author: joshsanyal
+
 import turtle
 
-t = turtle.Turtle()
-
-n = int (input("How many legs should this sprite have?"))
-print n
-
-t.shape("triangle")
-angle = 352 / n
-	
-for j in range(0,n):
-	t.right(angle)
-	t.forward(100)
-	t.stamp()
-	for n in range(18):
-		t.right(22)
-		t.stamp()
-	t.backward(100)
+t.turtle.Turtle()
+t.forward(100)
