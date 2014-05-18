ac126429a239f3f87a0cd1d308de640052b84ff8
joshsanyal <josh.sanyal@yahoo.com>
2014-05-18 14:31:25 -0700

    pythonroom update

--- drawshapes.py
+++ drawshapes.py
@@ -1,1 +1,117 @@
 # author: joshsanyal
+
+import turtle
+
+shape = input("What should I draw?")
+
+def makeShape(sides):
+	t = turtle.Turtle()
+	for a in range(sides):
+		t.forward(100)
+		t.left(360.000/sides)
+		
+if shape == "square":
+	makeShape(4)
+	
+if shape == "triangle":
+	makeShape(3)
+	
+if shape == "pentagon":
+	makeShape(5)
+	
+if shape == "hexagon":
+	makeShape(6)
+	
+if shape == "septagon":
+	makeShape(7)
+	
+if shape == "octagon":
+	makeShape(8)
+	
+if shape == "nonagon":
+	makeShape(9)
+		
+if shape == "decagon":
+	makeShape(10)
+	
+if shape == "decagon":
+	makeShape(11)
+	
+if shape == "decagon":
+	makeShape(12)
+	
+if shape == "decagon":
+	makeShape(13)
+		
+if shape == "decagon":
+	makeShape(14)
+		
+if shape == "decagon":
+	makeShape(15)
+		
+if shape == "decagon":
+	makeShape(16)
+		
+if shape == "decagon":
+	makeShape(17)
+		
+if shape == "decagon":
+	makeShape(18)
+		
+if shape == "decagon":
+	makeShape(19)
+		
+if shape == "decagon":
+	makeShape(20)
+		
+if shape == "decagon":
+	makeShape(21)
+		
+if shape == "decagon":
+	makeShape(22)
+		
+if shape == "decagon":
+	makeShape(23)
+		
+if shape == "decagon":
+	makeShape(24)
+		
+if shape == "decagon":
+	makeShape(25)
+		
+if shape == "decagon":
+	makeShape(26)
+		
+if shape == "decagon":
+	makeShape(27)
+		
+if shape == "decagon":
+	makeShape(28)
+		
+if shape == "decagon":
+	makeShape(29)
+		
+if shape == "decagon":
+	makeShape(30)
+		
+if shape == "decagon":
+	makeShape(31)
+		
+if shape == "decagon":
+	makeShape(32)
+		
+if shape == "decagon":
+	makeShape(33)
+		
+if shape == "decagon":
+	makeShape(34)
+	
+	
+def circle():
+	t = turtle.Turtle()
+	for c in range(180):
+		t.forward(3)
+		t.left(2)
+		
+if shape == "circle":
+	circle()
