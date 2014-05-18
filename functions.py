34f05cbb9f1749d79d3f5a4ebccd7a27fa155345
joshsanyal <josh.sanyal@yahoo.com>
2014-05-18 15:05:07 -0700

    pythonroom update

--- functions.py
+++ functions.py
@@ -15,9 +15,24 @@ def sayGoodbye():
 def sayGoodbyeTo(name):
 	print "see ya " + name + "."
 	
+def ageOf(person):
+	if person == "akhilesh":
+		return 9
+	if person == "ravi":
+		return 9
+	if person == "josh":
+		return 10
+	if person == "keshav":
+		return 28
+	if person == "yash":
+		return 31
+	
+
+
 sayHello()
 sayHelloTo("Keshav")
 sayGreeting("Hello ", "Akhilesh")
 sayGreeting("Hello ", "Josh")
 sayGreeting("Goodye ", "Ravi")
+print ageOf("josh") + ageOf("akhilesh") + ageOf("ravi") + ageOf("keshav") + ageOf("yash")
 sayGoodbye()
