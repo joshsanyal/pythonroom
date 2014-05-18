9f9fbd26a2c3ea87bae961b04cf07bdc82a52edd
joshsanyal <josh.sanyal@yahoo.com>
2014-05-18 14:50:59 -0700

    pythonroom update

--- functions.py
+++ functions.py
@@ -1,2 +1,23 @@
 # author: joshsanyal
 
+def sayHello():
+	print "Hello there!"
+	
+def sayHelloTo(name):
+	print "Hello " + name + "."
+	
+def sayGreeting(greeting, name):
+	print greeting + "there, " + name
+	
+def sayGoodbye():
+	print "See ya later!"
+	
+def sayGoodbyeTo(name):
+	print "see ya " + name + "."
+	
+sayHello()
+sayHelloTo("Keshav")
+sayGreeting("Hello ", "Akhilesh")
+sayGreeting("Hello ", "Josh")
+sayGreeting("Goodye ", "Ravi")
+sayGoodbye()
