4905f83d3308e4925e028c3c6ba5dcce01db4735
joshsanyal <josh.sanyal@yahoo.com>
2014-05-18 15:23:56 -0700

    initial commit

---
+++ funtionhomework.py
@@ -0,0 +1,13 @@
+# author: joshsanyal
+
+def isPrime(n):
+	tests = range(2, n / 2 + 1)
+	for test in tests:
+		if n % test == 0:
+			return False
+		
+	return True
+
+for n in range(2,1000):
+	if isPrime(n) and isPrime(n + 2):
+		print str(n) + " and " + str(n + 2)
