6863cce283ef19b12fdb39ae7488908e7c25b4ad
joshsanyal <josh.sanyal@yahoo.com>
2014-05-18 15:37:57 -0700

    pythonroom update

--- funtionhomework.py
+++ funtionhomework.py
@@ -1,13 +1,44 @@
 # author: joshsanyal
 
-def isPrime(n):
-	tests = range(2, n / 2 + 1)
-	for test in tests:
-		if n % test == 0:
-			return False
-		
-	return True
+def isGreaterThan100(n):
+	return false
 
-for n in range(2,1000):
-	if isPrime(n) and isPrime(n + 2):
-		print str(n) + " and " + str(n + 2)
+def whichIsGreater(a, b):
+	if a > b:
+		return a
+	else:
+		return b
+	
+def whichIsGreater(a, b, c):
+	return a
+
+def whichIsSmaller(a, b, c):
+	return a
+
+# Return whether a is divisible by b
+def divisibleBy(a, b):
+	return False
+
+# Simple functions, shouldm't take you more than 15 seconds
+def add(a, b):
+	return a
+def subtract(a, b):
+	return a
+def multiply(a, b):
+	return a
+def divide(a, b):
+	return a
+
+#interesting function: a to the power of b
+def power(a, b)):
+	return a
+def absoluteValue(a):
+	return a
+
+# challenge funstions
+# hint - use the modulus and division
+def palindromeNumber(n):
+	return alse
+
+def sumOfDigits(n):
+	return 0
