01c3973f10eddd40db332a495846a8a58cc1ea2d
joshsanyal <josh.sanyal@yahoo.com>
2014-05-25 15:40:04 -0700

    pythonroom update

--- HW.py
+++ HW.py
@@ -1,1 +1,12 @@
 # author: joshsanyal
+import random
+
+people = int(input("How many people are there?"))
+flies= int(input("How many flies are in the room?"))
+
+if random.randint(1,1000) < flies:
+	print "Someone is blinking."
+else:
+	print "Nobody is blinking."
+	
+pictures = 0
