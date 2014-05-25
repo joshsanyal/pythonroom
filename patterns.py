0b6d0dfd875b39c2d653f9f7617872c77ef1cf98
joshsanyal <josh.sanyal@yahoo.com>
2014-05-25 15:04:47 -0700

    pythonroom update

--- patterns.py
+++ patterns.py
@@ -1,7 +1,13 @@
 import turtle
-t = turtle.Turtle()
 
-t.setpos(-210,50)
-t.forward(420)
-t.setpos(-,50)
-t.stamp
+t = turtle.Turtle()
+colors = ["red", "green", "blue", "pink"]
+distances = range(10,100)
+for distance in distances:
+	t.left(15)
+	for c in colors:
+		t.color(c)
+		t.forward(distance)
+		t.left(90)
+		
+t.speed(0)
