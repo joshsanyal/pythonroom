f0faec06c97c4c01afedcd1a1e0d65856ab80519
joshsanyal <josh.sanyal@yahoo.com>
2014-06-01 13:30:34 -0700

    pythonroom update

--- strings.py
+++ strings.py
@@ -1,1 +1,9 @@
-# author: joshsanyal
+word = input("Speak, human")
+word = word.lower()
+output = ""
+vowels = ["a", "e", "i", "o", "u"]
+
+if word[0] == "a" or word[0] == "e" or word[0] == "i" or word[0] == "o" or word[0] == "u":
+	output += + word + "yay"
+else:
+	
