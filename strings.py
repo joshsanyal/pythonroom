056c61f71158630a265f5056dd40e41d96d22e5e
joshsanyal <josh.sanyal@yahoo.com>
2014-06-01 13:37:36 -0700

    pythonroom update

--- strings.py
+++ strings.py
@@ -1,9 +1,9 @@
 word = input("Speak, human")
 word = word.lower()
 output = ""
-vowels = ["a", "e", "i", "o", "u"]
 
 if word[0] == "a" or word[0] == "e" or word[0] == "i" or word[0] == "o" or word[0] == "u":
-	output += + word + "yay"
+	output += word + "yay"
 else:
-	
+	output += word[1,1000] + word[0] + "ay"	
+print output
