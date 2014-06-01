9dd6b4e70a83f7917f1df60920ade06eb9ca507d
joshsanyal <josh.sanyal@yahoo.com>
2014-06-01 15:36:27 -0700

    pythonroom update

--- HW6/1.py
+++ HW6/1.py
@@ -6,7 +6,6 @@ endOfWord = len(word) -1
 for i in range(length):
 	# Check if the word reads the same going forward and backward
 	
-	if word[0] == word[length]:
-		# do some code
+	if word[0] == word[length]":
 	else:
 		print "Not a palindrome"
