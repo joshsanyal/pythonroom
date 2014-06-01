21c74aa16476e80ad3fec70588cb47d0098a2f8e
joshsanyal <josh.sanyal@yahoo.com>
2014-06-01 14:39:15 -0700

    pythonroom update

--- strings.py
+++ strings.py
@@ -1,17 +1,21 @@
-word = input("Insert word for translation to pig latin")
-word = word.lower()
+words = input("Insert word for translation to pig latin")
+words = words.lower()
+words = words.split(" ")
 vowels = ["a", "e", "i", "o", "u"]
 output = ""
-
-if word[0] in vowels:
-	output += word + "yay"
-else:
-	start = 0
-	for letter in list(word):
-		if letter in vowels:
-			break
-		else:
-			start += 1
-	output += word[start:] + word[:start] + "ay"
+for word in words:
+	if word[0] in vowels:
+		output += word + "yay "
+	else:
+		start = 0
+		for letter in list(word):
+			if letter in vowels:
+				break
+			else:
+				if letter is "y":
+					break
+				else:
+					start += 1
+		output += word[start:] + word[:start] + "ay "
 
 print output
