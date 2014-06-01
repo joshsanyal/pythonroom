b0f0c042b9053468510c821da722c3d1387b6fb8
joshsanyal <josh.sanyal@yahoo.com>
2014-06-01 13:42:52 -0700

    pythonroom update

--- strings.py
+++ strings.py
@@ -5,5 +5,5 @@ output = ""
 if word[0] == "a" or word[0] == "e" or word[0] == "i" or word[0] == "o" or word[0] == "u":
 	output += word + "yay"
 else:
-	output += word[1,1000] + word[0] + "ay"	
+	output += word[1:] + word[0] + "ay"	
 print output
