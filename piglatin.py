171e139a13fb6c1af2a16ce31b488364519f626d
joshsanyal <josh.sanyal@yahoo.com>
2014-06-03 17:49:11 -0700

    initial commit

---
+++ piglatin.py
@@ -0,0 +1,20 @@
+words = input("Insert word/phrase for translation to pig latin")
+words = words.lower()
+words = words.split(" ")
+vowels = ["a", "e", "i", "o", "u"]
+output = ""
+for word in words:
+	if word[0] in vowels:
+		output += word + "yay "
+	else:
+		start = 0
+		for letter in list(word):
+			if letter in vowels:
+				break
+			else:
+				if letter is "y":
+					break
+				else:
+					start += 1
+		output += word[start:] + word[:start] + "ay "
+print output
