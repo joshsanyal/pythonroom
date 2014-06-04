15202eb32ee515abaa03b64c028064eeeff050bf
joshsanyal <josh.sanyal@yahoo.com>
2014-06-03 17:47:24 -0700

    pythonroom update

--- HW6/1.py
+++ HW6/1.py
@@ -3,9 +3,10 @@
 word = input("Speak, human.")
 StartOfWord = 0
 endOfWord = len(word) -1
-for i in range(length):
+for i in range(endOfWord):
 	# Check if the word reads the same going forward and backward
-	
-	if word[0] == word[length]":
+	start = 0
+	if word[start] = word[Endofword - start]:
+		start = start + 1
 	else:
 		print "Not a palindrome"
